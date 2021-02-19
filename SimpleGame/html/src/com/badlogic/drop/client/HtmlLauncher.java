f09deaedf3d9bceae2bf2b10d4d3809f32ad30cf
Wagner Franchin <wagner.franchin@oracle.com>
2021-02-19 10:09:06 +0000

    Added the simple game

---
+++ SimpleGame/html/src/com/badlogic/drop/client/HtmlLauncher.java
@@ -0,0 +1,20 @@
+package com.badlogic.drop.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.badlogic.drop.Drop;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Fixed size application:
+                return new GwtApplicationConfiguration(800, 480);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Drop();
+        }
+}
